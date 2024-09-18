143c65404acb511b73e79a67cd6ecb05cb72b3d9
Abdel780 <abdelkarim27200@gmail.com>
2024-09-18 11:53:50 +0200

    creation 404

---
+++ app/Views/404.view.php
@@ -0,0 +1,8 @@
+<?php
+    require_once(__DIR__."/partials/head.php")
+?>
+    <h1>ERREUR 404 </h1>
+    <a href="/">Retour à l'acueil</a>
+<?php
+    require_once(__DIR__."/partials/footer.php")
+?>
