9eb89a04f776fad32bbb3300abc67e9edc76ceca
Abdel780 <abdelkarim27200@gmail.com>
2024-09-19 13:53:59 +0200

    connexion deconnexion terminée

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -9,13 +9,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
                     <img src="/public/img/logoBMW.png" alt="BMW Logo" width="190" height="110" class="d-inline-block align-top">
-                    
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -26,18 +26,16 @@
                             <a class="nav-link active" aria-current="page" href="/">Accueil</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Models</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Discussions</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="/register">Inscription</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/connexion">Connexion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/deconnexion">Déconexion</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
     </header>
-
-
